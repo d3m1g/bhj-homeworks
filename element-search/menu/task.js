afaac73d9669890f776823b95802df0646380e5b
Alexander Sambur <demik86@mail.ru>
2023-04-13 22:17:11 +1000

    Исправил menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,11 +4,20 @@ const arr = Array.from(linkClick);
 
 for (let i = 0; i < arr.length; i++) {
  arr[i].onclick = (event) => {
+  const target = event.target;
+  if (target.parentElement.querySelector("ul") === null) {
+   return;
+  }
+
+  if (target.parentElement.querySelector("ul").className === "menu menu_sub menu_active") {
+   target.parentElement.querySelector("ul").className = "menu menu_sub";
+   return false;
+  }
+
   menuSub.forEach((element) => {
    element.className = "menu menu_sub";
   })
 
-  const target = event.target;
   target.parentElement.querySelector("ul").className = "menu menu_sub menu_active";
   return false;
  }
