f40100bcb9c7f8ec1a37f7bf72adaf47325d04d2
Alexander Sambur <demik86@mail.ru>
2023-07-20 03:04:43 +1000

    одна подсказка в момент времени работает

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,9 +1,19 @@
-const a = document.querySelectorAll('.has-tooltip');
+const a = document.getElementsByClassName('has-tooltip');
 const arr = Array.from(a);
+const active = document.getElementsByClassName('tooltip_active');
+
+function fun1() {
+ if (active.length === 1) {
+  active[0].className = "has-tooltip";
+ }   
+}
 
 arr.forEach((el) => {
- el.onclick = (event) => {  
-  el.classList.add('tooltip','tooltip_active');
-  event.preventDefault();
- };
-});
+ el.onclick = (event) => {
+  fun1();
+  el.classList.add('tooltip', 'tooltip_active');
+  el.innerText = el.title;
+  event.preventDefault();  
+ }
+}
+);
