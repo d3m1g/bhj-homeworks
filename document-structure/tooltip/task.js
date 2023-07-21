ca245daf4e4b329a408c850679c105df052acc58
Alexander Sambur <demik86@mail.ru>
2023-07-22 00:23:21 +1000

    Tooltip ready.

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,21 @@
 const a = document.getElementsByClassName('has-tooltip');
 const arr = Array.from(a);
 const active = document.getElementsByClassName('tooltip_active');
+let divTip = document.createElement('div');
 
 function fun1() {
  if (active.length === 1) {
-  active[0].className = "has-tooltip";
- }   
+  active[0].classList.remove('tooltip_active');
+ }
 }
 
 arr.forEach((el) => {
  el.onclick = (event) => {
   fun1();
-  el.classList.add('tooltip', 'tooltip_active');
-  el.innerText = el.title;
-  event.preventDefault();  
+  divTip.classList.add('tooltip', 'tooltip_active');
+  divTip.innerText = el.title;
+  event.preventDefault();
+  document.body.append(divTip);
  }
 }
 );
