307ac73291f0fccab210a4934ec3a81b8f34bf57
Alexander Sambur <demik86@mail.ru>
2023-07-26 01:21:29 +1000

    tooltip 26072023

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,26 +1,28 @@
 const a = document.getElementsByClassName('has-tooltip');
 const arr = Array.from(a);
+
 const active = document.getElementsByClassName('tooltip_active');
 let divTip = document.createElement('div');
 
 function fun1() {
- if (active.length === 1) {
-  active[0].classList.remove('tooltip_active');
- }
+ active[0].classList.remove('tooltip_active');
 }
 
 arr.forEach((el) => {
  el.onclick = (event) => {
-  fun1();
-  divTip.classList.add('tooltip', 'tooltip_active');
-  divTip.innerText = el.title;
-  event.preventDefault();
+  if (active[0] !== undefined) {
+   fun1();
+  } else {
+   divTip.classList.add('tooltip', 'tooltip_active');
+   divTip.innerText = el.title;
+   event.preventDefault();
 
-  let tipPosition = el.getBoundingClientRect();
-  divTip.style.top = tipPosition.top + 22 + 'px';
-  divTip.style.left = tipPosition.left + 'px';
+   let tipPosition = el.getBoundingClientRect();
+   divTip.style.top = tipPosition.top + 22 + 'px';
+   divTip.style.left = tipPosition.left + 'px';
 
-  document.body.append(divTip);
+   document.body.append(divTip);
+  }
  }
 }
 );
