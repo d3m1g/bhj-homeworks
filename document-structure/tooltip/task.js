a6798a094fac50c043a053e6ccd94ec46603f54f
Alexander Sambur <demik86@mail.ru>
2023-07-27 01:18:20 +1000

    tooltip 27072023

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,13 +5,14 @@ const active = document.getElementsByClassName('tooltip_active');
 let divTip = document.createElement('div');
 
 function fun1() {
- active[0].classList.remove('tooltip_active');
+ active[0].classList.toggle('tooltip_active');
 }
 
 arr.forEach((el) => {
  el.onclick = (event) => {
-  if (active[0] !== undefined) {
+  if (active.length > 0 && active[0].innerText === el.title) {
    fun1();
+   event.preventDefault();
   } else {
    divTip.classList.add('tooltip', 'tooltip_active');
    divTip.innerText = el.title;
